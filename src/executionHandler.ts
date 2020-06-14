c02ff7ee44919349dc6c8cc2c218687fb77cecaf
Adam Williams <adam.williams@lifeomic.com>
2020-06-14 16:37:02 -0400

    Break up work to clear memory between types

--- src/executionHandler.ts
+++ src/executionHandler.ts
@@ -24,6 +24,7 @@ import {
   ACCOUNT_DEVICE_SENSOR_RELATIONSHIP_TYPE,
   ACCOUNT_ENTITY_TYPE,
   ACCOUNT_SERVICE_RELATIONSHIP_TYPE,
+  CbDefenseExecutionContext,
   DEVICE_SENSOR_ENTITY_TYPE,
   SENSOR_DEVICE_RELATIONSHIP_TYPE,
   SERVICE_ENTITY_TYPE,
@@ -32,7 +33,8 @@ import {
 export default async function executionHandler(
   context: IntegrationExecutionContext,
 ): Promise<IntegrationExecutionResult> {
-  const { graph, persister, provider } = initializeContext(context);
+  const cbContext = initializeContext(context);
+  const { provider } = cbContext;
 
   const results: PersisterOperationsResult[] = [];
 
@@ -43,27 +45,22 @@ export default async function executionHandler(
     accountData.organization_id,
   );
 
-  const [
-    oldAccountEntities,
-    oldServiceEntities,
-    oldAccountServiceRelationships,
-  ] = await Promise.all([
-    graph.findAllEntitiesByType(ACCOUNT_ENTITY_TYPE),
-    graph.findEntitiesByType(SERVICE_ENTITY_TYPE),
-    graph.findRelationshipsByType(ACCOUNT_SERVICE_RELATIONSHIP_TYPE),
-  ]);
-
   results.push(
-    await persister.publishPersisterOperations([
-      [
-        ...persister.processEntities(oldAccountEntities, [newAccountEntity]),
-        ...persister.processEntities(oldServiceEntities, [newServiceEntity]),
-      ],
-      persister.processRelationships(oldAccountServiceRelationships, [
-        createAccountServiceRelationship(newAccountEntity, newServiceEntity),
-      ]),
-    ]),
+    await syncAccountAndService(cbContext, newAccountEntity, newServiceEntity),
   );
+  results.push(await syncDeviceSensors(cbContext, newAccountEntity));
+  results.push(await syncAlertFindings(cbContext));
+
+  return {
+    operations: summarizePersisterOperationsResults(...results),
+  };
+}
+
+async function syncDeviceSensors(
+  context: CbDefenseExecutionContext,
+  newAccountEntity: EntityFromIntegration,
+): Promise<PersisterOperationsResult> {
+  const { provider, graph, persister } = context;
 
   const newDeviceSensorEntities: EntityFromIntegration[] = [];
   const newAccountDeviceSensorRelationships: IntegrationRelationship[] = [];
@@ -81,7 +78,6 @@ export default async function executionHandler(
       mapSensorToDeviceRelationship(newDeviceSensorEntity),
     );
   });
-
   const [
     oldDeviceSensorEntities,
     oldAccountDeviceSensorRelationships,
@@ -91,25 +87,51 @@ export default async function executionHandler(
     graph.findRelationshipsByType(ACCOUNT_DEVICE_SENSOR_RELATIONSHIP_TYPE),
     graph.findRelationshipsByType(SENSOR_DEVICE_RELATIONSHIP_TYPE),
   ]);
-
-  results.push(
-    await persister.publishPersisterOperations([
-      persister.processEntities(
-        oldDeviceSensorEntities,
-        newDeviceSensorEntities,
+  return persister.publishPersisterOperations([
+    persister.processEntities(oldDeviceSensorEntities, newDeviceSensorEntities),
+    [
+      ...persister.processRelationships(
+        oldAccountDeviceSensorRelationships,
+        newAccountDeviceSensorRelationships,
       ),
-      [
-        ...persister.processRelationships(
-          oldAccountDeviceSensorRelationships,
-          newAccountDeviceSensorRelationships,
-        ),
-        ...persister.processRelationships(
-          oldMappedDeviceRelationships,
-          newMappedDeviceRelationships,
-        ),
-      ],
+      ...persister.processRelationships(
+        oldMappedDeviceRelationships,
+        newMappedDeviceRelationships,
+      ),
+    ],
+  ]);
+}
+
+async function syncAccountAndService(
+  context: CbDefenseExecutionContext,
+  newAccountEntity: EntityFromIntegration,
+  newServiceEntity: EntityFromIntegration,
+): Promise<PersisterOperationsResult> {
+  const { graph, persister } = context;
+  const [
+    oldAccountEntities,
+    oldServiceEntities,
+    oldAccountServiceRelationships,
+  ] = await Promise.all([
+    graph.findAllEntitiesByType(ACCOUNT_ENTITY_TYPE),
+    graph.findEntitiesByType(SERVICE_ENTITY_TYPE),
+    graph.findRelationshipsByType(ACCOUNT_SERVICE_RELATIONSHIP_TYPE),
+  ]);
+  return persister.publishPersisterOperations([
+    [
+      ...persister.processEntities(oldAccountEntities, [newAccountEntity]),
+      ...persister.processEntities(oldServiceEntities, [newServiceEntity]),
+    ],
+    persister.processRelationships(oldAccountServiceRelationships, [
+      createAccountServiceRelationship(newAccountEntity, newServiceEntity),
     ]),
-  );
+  ]);
+}
+
+async function syncAlertFindings(
+  context: CbDefenseExecutionContext,
+): Promise<PersisterOperationsResult> {
+  const { provider, persister } = context;
 
   const alertsSinceDate = await determineAlertsSinceDate(
     context.clients.getClients().integrationService,
@@ -125,16 +147,10 @@ export default async function executionHandler(
     );
   }, alertsSinceDate);
 
-  results.push(
-    await persister.publishPersisterOperations([
-      persister.processEntities([], recentAlertFindingEntities),
-      persister.processRelationships([], deviceSensorAlertFindingRelationships),
-    ]),
-  );
-
-  return {
-    operations: summarizePersisterOperationsResults(...results),
-  };
+  return persister.publishPersisterOperations([
+    persister.processEntities([], recentAlertFindingEntities),
+    persister.processRelationships([], deviceSensorAlertFindingRelationships),
+  ]);
 }
 
 async function determineAlertsSinceDate(
